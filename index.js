06d810facc11b424998db5c9474aa0c809a08d6d
erik210282 <erik@dhsc.co.kr>
2025-05-28 11:43:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
